2f8e2983b0450c32a40dcc35f639f8084b011b94
Akshay <waizaeer4@gmail.com>
2020-05-21 00:21:26 +0530

    Login Navigation Created and UI is working

--- app/src/main/java/org/wazir/build/elemenophee/MainActivity.java
+++ app/src/main/java/org/wazir/build/elemenophee/MainActivity.java
@@ -1,14 +1,20 @@
 package org.wazir.build.elemenophee;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
+    CardView cardView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
